c0e2dc381bf845ee20fdcb0ace6cd04fd05e3805
raden rangga pratama <108499855+darisRanggaPratama@users.noreply.github.com>
2023-10-14 16:39:47 +0700

    initial commit

---
+++ basic/crud_whseet/comment/rg_error_clr_cmt_toex.py
@@ -0,0 +1,51 @@
+import openpyxl as pyxl
+from openpyxl.styles import PatternFill
+from openpyxl.comments import Comment
+
+# Buka file Excel: *.xlsx
+path = input('Error Values in Worksheet\nFile Name: ')
+workbook = pyxl.load_workbook(path)
+
+# Pilih worksheet tertentu
+print(f'Available Worksheet:\n{workbook.sheetnames}')
+sheet = input('Get 1 Worksheet: ')
+worksheet = workbook[sheet]
+
+# Inisialisasi list untuk menyimpan hasil
+result = []
+
+# Warna latar belakang
+colored = PatternFill(start_color='EDBB99', end_color='EDBB99', fill_type='solid')
+
+# Loop melalui sel di Worksheet target, misal dari A2 sampai F46
+for row in worksheet.iter_rows(min_row=2, max_row=46, min_col=1, max_col=6):
+    for cell in row:
+        # Periksa apakah nilai Error
+        if isinstance(cell.value, str):
+            # Cek panjang nilai cell
+            if len(cell.value) > 0:
+                # Cek karakter dicari
+                if cell.value.startswith('#'):
+                    commented = Comment(f"ERROR\n{cell.coordinate}: {cell.value}", "Author")
+                    cell.comment = None
+                    cell.comment = commented
+                    cell.fill = colored
+                    # Tambahkan hasil ke list
+                    result.append((cell.coordinate, cell.value))
+
+# Buat worksheet "ERR" jika belum ada
+if 'ERR' not in workbook.sheetnames:
+    workbook.create_sheet('ERR')
+
+# Pilih worksheet "ERR"
+sheetErr = workbook['ERR']
+
+# Judul: ERROR
+sheetErr.cell(row=1, column=2, value='No; Cell; ERROR')
+
+# Tulis hasil ke dalam Worksheet "ERR" di cell B2
+for i, (coordinate, value) in enumerate(result):
+    sheetErr.cell(row=i + 2, column=2, value=f"{i + 1}; {coordinate}; {value}")
+
+# Simpan perubahan ke dalam file Excel: *.xlsx
+workbook.save('sample.xlsx')
--- basic/crud_whseet/comment/rg_null_clr_cmt_toex.py
+++ basic/crud_whseet/comment/rg_null_clr_cmt_toex.py
@@ -3,7 +3,7 @@ from openpyxl.styles import PatternFill
 from openpyxl.comments import Comment
 
 # Buka file Excel: *.xlsx
-path = input('Decimal Values in Worksheet\nFile Name: ')
+path = input('Null Values in Worksheet\nFile Name: ')
 workbook = pyxl.load_workbook(path)
 
 # Pilih worksheet tertentu
@@ -15,32 +15,33 @@ worksheet = workbook[sheet]
 result = []
 
 # Warna latar belakang
-colored = PatternFill(start_color='F9E79F', end_color='F9E79F', fill_type='solid')
+colored = PatternFill(start_color='85C1E9', end_color='85C1E9', fill_type='solid')
 
 # Loop melalui sel di Worksheet target, misal dari A2 sampai F46
 for row in worksheet.iter_rows(min_row=2, max_row=46, min_col=1, max_col=6):
     for cell in row:
-        # Periksa apakah nilai sel adalah Decimal/Float/Double
-        if isinstance(cell.value, float):
-            commented = Comment(f"DECIMAL\n{cell.coordinate}: {cell.value}", "Author")
+        # Periksa apakah nilai Blank/ Null
+        if cell.value is None:
+            commented = Comment(f"NULL\n{cell.coordinate}: {cell.value}", "Author")
             cell.comment = None
             cell.comment = commented
             cell.fill = colored
+            # Tambahkan hasil ke list
             result.append((cell.coordinate, cell.value))
 
-# Buat worksheet "DEC" jika belum ada
-if 'DEC' not in workbook.sheetnames:
-    workbook.create_sheet('DEC')
+# Buat worksheet "NULL" jika belum ada
+if 'NUL' not in workbook.sheetnames:
+    workbook.create_sheet('NUL')
 
-# Pilih worksheet "DEC"
-sheetDec = workbook['DEC']
+# Pilih worksheet "NULL"
+sheetNul = workbook['NUL']
 
-# Judul: Desimal
-sheetDec.cell(row=1, column=2, value='No; Cell; DECIMAL')
+# Judul: NULL
+sheetNul.cell(row=1, column=2, value='No; Cell; NULL')
 
-# Tulis hasil ke dalam Worksheet "DEC" di cell B2
+# Tulis hasil ke dalam Worksheet "NULL" di cell B2
 for i, (coordinate, value) in enumerate(result):
-    sheetDec.cell(row=i + 2, column=2, value=f"{i + 1}; {coordinate}; {value}")
+    sheetNul.cell(row=i + 2, column=2, value=f"{i + 1}; {coordinate}; {value}")
 
-# Simpan perubahan ke dalam file.xlsx
+# Simpan perubahan ke dalam file Excel: *.xlsx
 workbook.save('sample.xlsx')
